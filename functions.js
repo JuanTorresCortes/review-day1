689eb44360cb802ff82b101dd17598f275ea4ed2
Juan <juan.giana8589@gmail.com>
2023-05-16 08:33:37 -0500

    1st commit

--- functions.js
+++ functions.js
@@ -2,40 +2,103 @@ const returnEmptyString = function () {
 	return "";
 };
 
-const returnZeroNumber = function () {};
+const returnZeroNumber = function () {
+	return 0;
+};
 
-const returnEmptyArray = function () {};
+const returnEmptyArray = function () {
+	return [];
+};
 
-const returnEmptyObject = function () {};
+const returnEmptyObject = function () {
+	return {};
+};
 
-const returnAbcString = function (value) {};
+const returnAbcString = function (value) {
+	return value;
+};
 
-const returnSumOfTwo = function (arg1, arg2) {};
 
-const subtraction = function (arg1, arg2) {};
+const returnSumOfTen = function (arg1, arg2) {
+	let result = arg1 + arg2;
+	return result;	
+};
 
-const multiplication = function (arg1, arg2) {};
+const subtraction = function (arg1, arg2) {
+	let result = arg1 - arg2;
+	return result;
+};
 
-const division = function (arg1, arg2) {};
+const multiplication = function (arg1, arg2) {
+	let result = arg1 * arg2;
+	return result;
+};
 
-const returnArray = function (arg1) {};
+const division = function (arg1, arg2) {
+	let result = arg1 / arg2;
+	return result;
+};
 
-const returnFirstIndex = function (arg1) {};
+const returnArray = function (arg1) {
+	return arg1;
+};
 
-const returnSecondIndex = function (arg1) {};
+const returnFirstIndex = function (arg1) {
+	let first = arg1[0];
+	return first;
+};
 
-const returnArrayLength = function (arg1) {};
+const returnSecondIndex = function (arg1) {
+	let second = arg1[1];
+	return second;
+};
 
-const arraySum = function (arg1) {};
+const returnArrayLength = function (arg1) {
+	let length = arg1.length
+	return length;
+};
 
-const arraySubtraction = function (arg1) {};
+const arraySum = function (arg1) {
+	const sumArr = arg1.reduce((acc, value) => 
+		acc + value, 0
+	)
+	return sumArr;
+};
 
-const multiplicationArray = function (arg1) {};
+const arraySubtraction = function (arg1) {
+	const subtractArr = arg1.reduce((acc, value) => 
+		acc - value, 0
+	)
+	return subtractArr;
+};
 
-const divisionArray = function (arg1) {};
+const multiplicationArray = function (arg1) {
+	const multiplyArr = arg1.reduce((acc, value) => 
+		acc * value, 1
+	)
+	return multiplyArr;
+};
 
-const oddArray = function (arg1) {};
+const divisionArray = function (arg1) {
+	const divisionArr = arg1.reduce((acc, value) => 
+		acc / value, 1
+	)
+	let result = Math.round(divisionArr * 1000)/1000;
+	return result
+};
 
-const evenArray = function (arg1) {};
+const oddArray = function (arg1) {
+	let result = arg1.filter(num => num %2 !== 0);
+	return result
+};
 
-const returnFruits = function (arg1) {};
+const evenArray = function (arg1) {
+	let result = arg1.filter(num => num %2 === 0);
+	return result
+};
+
+const returnFruits = function (arg1) {
+	let leftFruit = arg1[0];
+	let rightFruit = arg1[arg1.length -1];
+	return [leftFruit,rightFruit];
+};
